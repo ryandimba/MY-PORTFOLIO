db935fdfed43ccb900b4e540ad2f567678d7433a
ryandimba <105648963+ryandimba@users.noreply.github.com>
2024-02-19 23:19:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+<script src="script.js">
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can add code to handle form submission, like sending an email
+    alert('Message sent!');
+  });
+  </script>
